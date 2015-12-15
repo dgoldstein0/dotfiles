4b30277fcea74fbf84c95c688be95ed2b46e1aa1
David Goldstein <dgoldstein@dropbox.com>
2015-12-15 01:30:38 -0800

    get rid of errors in the init script

--- init.sh
+++ init.sh
@@ -12,6 +12,20 @@ fi
 
 # Installation.  Symlinks on unix, junction points on windows.
 # But junctions only work on folders, so copy the files for windows.
+function link() {
+    local SOURCE=$2;
+    local TARGET=$1;
+    if [[ -e $SOURCE ]]; then
+        if [[ $SOURCE -ef $TARGET ]]; then
+            return;
+        else
+            echo "$SOURCE already exists, can't link it to $TARGET";
+        fi
+    else
+        ln -s $TARGET $SOURCE;
+    fi
+}
+
 if [[ $WINDOWS == 1 ]]; then
     junction ~/.vim/ ~/settings_files/.vim/;
     cp ~/settings_files/vimrc ~/.vimrc
@@ -25,12 +39,12 @@ else
     # ln decides to make a symlink from ~/.vim/.vim to ~/settings_files/.vim
     # for some reason that's completely beyond me.  symlinks to files don't
     # seem to have this problem.
-    ln -n -s ~/settings_files/.vim ~/.vim;
-    ln -s ~/settings_files/vimrc ~/.vimrc;
-    ln -s ~/settings_files/inputrc ~/.inputrc;
-    ln -n -s ~/settings_files/ipython_profile ~/.ipython/profile_default
-    ln -s ~/settings_files/.gitconfig ~/.gitconfig;
-    ln -s ~/settings_files/gitconfig_global ~/.gitconfig_global;
+    link ~/settings_files/.vim ~/.vim;
+    link ~/settings_files/vimrc ~/.vimrc;
+    link ~/settings_files/inputrc ~/.inputrc;
+    link ~/settings_files/ipython_profile ~/.ipython/profile_default
+    link ~/settings_files/.gitconfig ~/.gitconfig;
+    link ~/settings_files/gitconfig_global ~/.gitconfig_global;
 fi
 
 git submodule init
