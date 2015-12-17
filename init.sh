17726fed6d90d99f41a77b3ec37405beb396b90d
David Goldstein <dgoldstein0@gmail.com>
2015-12-17 05:04:44 -0800

    add options to init.sh.  Make install directory optional (defaults to homedir) and create an installation mode which copies files instead of symlinking.

--- init.sh
+++ init.sh
@@ -3,6 +3,45 @@
 # NOTE: this script should be idempotent, so it can be used to update
 # settings to the latest and not just to init a new machine.
 
+# param parsing: based on http://stackoverflow.com/questions/7069682/how-to-get-arguments-with-flags-in-bash-script
+
+MODE="link"
+INSTALL_DIR=$HOME
+
+while test $# -gt 0; do
+    case "$1" in
+        -h|--help)
+            cat << EOF
+Script to install / update dotfiles on the current machine.  When run in link mode (default), it only needs to be run to pick up new files / configuration changes; in copy mode, it should be run on every pull.
+
+usage: ./init.sh [options]
+
+options:
+--copy: copy the dotfiles to the target directory instead of symlinking them.
+--install-dir: the directory to put all the dotfiles into.  Defaults to the homedir.
+EOF
+            exit 0
+            ;;
+        --copy)
+            MODE="copy"
+            shift
+            ;;
+        --install-dir)
+            shift
+            if test $# -gt 0; then
+                INSTALL_DIR=$1
+            else
+                echo "no install dir specified"
+                exit 1
+            fi
+            shift
+            ;;
+        *)
+            echo "unrecognized option $1"
+            exit 1
+    esac
+done
+
 # OS detection first
 if [[ "$OSTYPE" == "msys" || "$OSTYPE" == "cygwin" ]]; then
     WINDOWS=1;
@@ -35,7 +74,7 @@ function link() {
         else
             echo "$SOURCE already exists, can't link it to $TARGET";
         fi
-    else
+    elif [[ $MODE == "link" ]]; then
         if [[ $WINDOWS == 1 ]]; then
             if [[ -d $TARGET ]]; then
                 echo "creating junction from $SOURCE to $TARGET"
@@ -49,23 +88,33 @@ function link() {
         else
             ln -s $TARGET $SOURCE;
         fi
+    else # MODE is copy
+        if [[ -d $TARGET ]]; then
+            cp -R $TARGET $SOURCE
+        else
+            cp $TARGET $SOURCE
+        fi
     fi
 }
 
-link ~/settings_files/.vim ~/.vim;
-link ~/settings_files/vimrc ~/.vimrc;
-link ~/settings_files/inputrc ~/.inputrc;
-mkdir -p ~/.ipython
-link ~/settings_files/ipython_profile ~/.ipython/profile_default
-link ~/settings_files/.gitconfig ~/.gitconfig;
-link ~/settings_files/gitconfig_global ~/.gitconfig_global;
+# do this first so it happens before any copying of directories
+git submodule init
+git submodule update
+
+link ~/settings_files/.vim $INSTALL_DIR/.vim;
+link ~/settings_files/vimrc $INSTALL_DIR/.vimrc;
+link ~/settings_files/inputrc $INSTALL_DIR/.inputrc;
+mkdir -p $INSTALL_DIR/.ipython
+link ~/settings_files/ipython_profile $INSTALL_DIR/.ipython/profile_default
+link ~/settings_files/.gitconfig $INSTALL_DIR/.gitconfig;
+link ~/settings_files/gitconfig_global $INSTALL_DIR/.gitconfig_global;
 
 # create empty ~/.gitconfig_local if it doesn't exist
-if [[ ! -e ~/.gitconfig_local ]]; then
-    touch ~/.gitconfig_local;
+if [[ ! -e $INSTALL_DIR/.gitconfig_local ]]; then
+    touch $INSTALL_DIR/.gitconfig_local;
 fi
 
-if [[ $(git config --get user.email) = "" ]]; then
+if [[ $(git config -f $INSTALL_DIR/.gitconfig_local --get user.email) = "" ]]; then
     echo "What do you want as the default git email for this machine?"
     read EMAIL;
     if [ $? -ne 0 ]; then
@@ -73,9 +122,7 @@ if [[ $(git config --get user.email) = "" ]]; then
         exit 1;
     else
         echo "setting email $EMAIL";
-        git config -f ~/.gitconfig_local user.email "$EMAIL";
+        git config -f $INSTALL_DIR/.gitconfig_local user.email "$EMAIL";
     fi
 fi
 
-git submodule init
-git submodule update
